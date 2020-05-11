aae347906bc498ba4c0f9f1b4d1582cb46158c21
rsarujan <rajaratnam.sarujan@gmail.com>
2020-05-11 09:31:19 +0200

    11/05/2020

--- MVC_new/Views/view_home.php
+++ MVC_new/Views/view_home.php
@@ -6,14 +6,14 @@ require_once 'view_begin.php';
 ?>
 
 	<main>
-		<h1> Liste des individus</h1>
+		<h1>Bienvenue sur le site web de recensement des étudiants</h1>
 
 		<p> Bienvenue sur le site web qui recense les différents individus de l'Université Paris Nanterre. Il y en a  <strong>
 			<?php 
 			
 			echo $nb;
 			?>	
-		</strong> individus. <br>Vous pouvez modifier cette liste en ajoutant de nouveaux individus, et en supprimant ou en mettant à jour les informations contenues dans cette base de données. Vous pouvez également ajouter, modifier, supprimer un nouveau groupe, un statut, un annuaire. <br><br>Dans un prochain proche, vous pourrez effectuer une recherche parmi les individus.... </p>
+		</strong> individus. <br>Vous pouvez modifier cette liste en ajoutant de nouveaux individus, en supprimant ou en mettant à jour les informations contenues dans cette base de données. Vous pouvez également ajouter, modifier, supprimer un nouveau groupe, attribuer un individu à un groupe, etc... <br><br>Dans un prochain proche, vous pourrez effectuer une recherche parmi les individus.... </p>
 	</main>
 </body>
 </html>
